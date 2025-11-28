0633ff5258678eff65d5e88209d1d8a2dc7ab7f3
Gytis Taločka <gytis.talocka@saldo.com>
2025-11-28 17:34:17 +0200

    Checkered image with 2 colors

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,47 @@
 const std = @import("std");
-const ppm_zig = @import("ppm_zig");
+
+const Color = extern struct {
+    r: u8 = 0,
+    g: u8 = 0,
+    b: u8 = 0,
+};
+
+const light = Color{ .r = 55, .g = 11, .b = 69 };
+const dark = Color{ .r = 203, .g = 163, .b = 255 };
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try ppm_zig.bufferedPrint();
-}
+    const cwd: std.fs.Dir = std.fs.cwd();
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
+    cwd.makeDir("output") catch |e| switch (e) {
+        error.PathAlreadyExists => {},
+        else => return e,
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+
+    var output_dir: std.fs.Dir = try cwd.openDir("output", .{});
+    defer output_dir.close();
+
+    const file: std.fs.File = try output_dir.createFile("image.ppm", .{});
+    defer file.close();
+
+    const image_h: u8 = 100;
+    const image_w: u8 = 100;
+
+    var output_writer: std.fs.File.Writer = file.writer(&.{});
+    const writer: *std.Io.Writer = &output_writer.interface;
+
+    _ = try writer.print("P6\n{d} {d}\n255\n", .{ image_w, image_h });
+
+    for (0..image_h) |y| {
+        for (0..image_w) |x| {
+            var cell_color: Color = undefined;
+            if ((x / 10 + y / 10) % 2 == 0) {
+                cell_color = light;
+            } else {
+                cell_color = dark;
+            }
+            _ = try writer.print("{s}", .{std.mem.asBytes(&cell_color)});
+        }
+    }
+
+    _ = try writer.flush();
 }

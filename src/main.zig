9b258538df80a02097fc56bbed3fcb3ad89457ca
Gytis Talocka <kurideja@gmail.com>
2025-11-30 17:11:25 +0200

    checkered board frames

--- src/main.zig
+++ src/main.zig
@@ -11,32 +11,42 @@ pub fn main() !void {
     var output_dir = try cwd.openDir("output", .{});
     defer output_dir.close();
 
-    var file = try output_dir.createFile("image.ppm", .{
-        .truncate = true,
-        .read = false,
-    });
-    defer file.close();
-
     const image_w: usize = 100;
     const image_h: usize = 100;
-
-    var buf: [4096]u8 = undefined; // or use &.{} for unbuffered
-    var file_writer: std.fs.File.Writer = file.writer(&buf);
-    const writer: *std.Io.Writer = &file_writer.interface;
-
-    try writer.print("P6\n{d} {d}\n255\n", .{ image_w, image_h });
 
     const light: [3]u8 = .{ 200, 200, 200 };
     const dark: [3]u8 = .{ 50, 50, 50 };
 
-    for (0..image_h) |y| {
-        for (0..image_w) |x| {
-            const cell_color =
-                if ((x / 10 + y / 10) % 2 == 0) light else dark;
+    for (0..100) |i| {
+        var file_name_buf: [64]u8 = undefined; // temp buffer for filename
 
-            try writer.writeAll(std.mem.asBytes(&cell_color));
+        const filename = try std.fmt.bufPrint(
+            &file_name_buf,
+            "output-{d:0>2}.ppm", // {d:0>2} = zero-padded width 2
+            .{i},
+        );
+
+        var file = try output_dir.createFile(filename, .{
+            .truncate = true,
+            .read = false,
+        });
+        defer file.close();
+
+        var buf: [4096]u8 = undefined; // or use &.{} for unbuffered
+        var file_writer: std.fs.File.Writer = file.writer(&buf);
+        const writer: *std.Io.Writer = &file_writer.interface;
+
+        try writer.print("P6\n{d} {d}\n255\n", .{ image_w, image_h });
+
+        for (0..image_h) |y| {
+            for (0..image_w) |x| {
+                const cell_color =
+                    if (((x + i) / 10 + (y + i) / 10) % 2 == 0) light else dark;
+
+                try writer.writeAll(std.mem.asBytes(&cell_color));
+            }
         }
-    }
 
-    try writer.flush();
+        try writer.flush();
+    }
 }
